a45d7a5aa9c70d36629118466f5f311dc3490750
planner-sohrab <plannersohrab@gmail.com>
2022-12-08 11:35:42 +0600

    final output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component.jsx'
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -19,30 +20,30 @@ class App extends Component {
         return { monsters: users };
       }));
   }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
 
 
   render() {
-    const filteredMonsters = this.state.monsters.filter(
-      monster => {
-        return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-      }
-    );
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this
 
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
     return (
       <div className='App'>
-        <input className='search-box' type='search' placeholder='search-monsters' onChange={event => {
-          const searchField = event.target.value.toLocaleLowerCase();
+        <h1 className="app-title">Monster Rolodex</h1>
+        <SearchBox
+          className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters' />
+        <CardList monsters={filteredMonsters} />
 
-          this.setState(() => {
-            return { searchField }
-          });
-        }} />
-        {filteredMonsters.map(monster => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1></div>
-          )
-        })}
       </div>
     );
   }
